c601ae823010f6a1268bff0c5bee2d04fa0ac68e
Miten Jain <miten@soe.ucsc.edu>
2015-01-08 13:37:10 -0800

    formatting

--- tests/tests.py
+++ tests/tests.py
@@ -10,21 +10,15 @@ def validateSam(samFile):
     """
     # Specify a flag to check if whether samfile had a header
     fileFlag = "None"
-    
     # Check if samfile exists
-    if not os.path.isfile(samFile) is True:
-        print "No samfile found, terminating test"
-        sys.exit()
-
+    self.assertTrue(os.path.isfile(samFile))
     # Try opening samfile
     samfile = pysam.Samfile(samFile, "r" )
     # Check if samfile contains a proper header, and set fileFlag accordingly
     if samfile.header:
         if "HD" and "SQ" in samfile.header:
             fileFlag = "success"
-
     samfile.close()
-
     if fileFlag != "None":
         print "Samfile validation successful"
     else:
@@ -40,49 +34,32 @@ class TestCase(unittest.TestCase):
         unittest.TestCase.tearDown(self)
 
     def testMarginAlignNoEm(self):
-
         print "Running marginAlign"
         os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --jobTree testJobTree")
-
         # Validate samfile
         validateSam("./tests/test.sam")
-
         # Clean up
         print "Cleaning files"
         os.system("rm -rf tests/test.sam testJobTree")
 
-
     def testMarginAlignEm(self):
-
         print "Running marginAlign and writing output.hmm"
         os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --em --outputModel tests/output.hmm --jobTree testJobTree")
-
         # Validate samfile
         validateSam("./tests/test.sam")
-
         # Check if output.hmm files were written
         self.assertTrue(os.path.isfile("./tests/output.hmm"))
-
         # Clean up
         print "Cleaning files"
         os.system("rm -rf tests/test.sam testJobTree tests/output.hmm*")
 
-
     def testMarginAlignReadModel(self):
-
         # Check if input.hmm exists
-        if os.path.isfile("./tests/input.hmm"):
-            print "Input hmm file found"
-        else:
-            print "No input hmm file, terminating test"
-            sys.exit()
-
+        self.assertTrue(os.path.isfile("./tests/input.hmm"))
         print "Running marginAlign and reading model from input.hmm"
         os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --inputModel tests/input.hmm --jobTree testJobTree")
-
         # Validate samfile
         validateSam("./tests/test.sam")
-
         # Clean up
         print "Cleaning files"
         os.system("rm -rf tests/test.sam testJobTree")
