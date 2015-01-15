94f2acb90f6ef11cf25797938cbf36a18807f535
Miten Jain <miten@soe.ucsc.edu>
2015-01-15 09:18:40 -0800

    enabling tests, need to improve code

--- tests/tests.py
+++ tests/tests.py
@@ -39,49 +39,49 @@ class TestCase(unittest.TestCase):
         self.validateSam("./tests/test.sam")
         # Clean up
         print "Cleaning files"
-#        os.system("rm -rf tests/test.sam testJobTree")
+        os.system("rm -rf tests/test.sam testJobTree")
 
-#    def testMarginAlignEm(self):
-#        print "Running marginAlign and writing output.hmm"
-#        os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --em --outputModel tests/output.hmm --jobTree testJobTree")
-#        # Validate samfile
-#        self.validateSam("./tests/test.sam")
-#        # Check if output.hmm files were written
-#        self.assertTrue(os.path.isfile("./tests/output.hmm"))
-#        # Clean up
-#        print "Cleaning files"
-#        os.system("rm -rf tests/test.sam testJobTree tests/output.hmm*")
-
-#    def testMarginAlignReadModel(self):
-#        # Check if input.hmm exists
-#        self.assertTrue(os.path.isfile("./tests/input.hmm"))
-#        print "Running marginAlign and reading model from input.hmm"
-#        os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --inputModel tests/input.hmm --jobTree testJobTree")
+    def testMarginAlignEm(self):
+        print "Running marginAlign and writing output.hmm"
+        os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --em --outputModel tests/output.hmm --jobTree testJobTree")
         # Validate samfile
-#        self.validateSam("./tests/test.sam")
-#        # Clean up
-#        print "Cleaning files"
-#        os.system("rm -rf tests/test.sam testJobTree")
+        self.validateSam("./tests/test.sam")
+        # Check if output.hmm files were written
+        self.assertTrue(os.path.isfile("./tests/output.hmm"))
+        # Clean up
+        print "Cleaning files"
+        os.system("rm -rf tests/test.sam testJobTree tests/output.hmm*")
 
-#    def testMarginCaller(self):
-#        # Validate input.sam
-#        self.validateSam("./tests/input.sam")
-#        print "Running marginCaller"
-#        os.system("./marginCaller tests/input.sam tests/reference.fa tests/output.vcf --jobTree testJobTree")
-#        self.assertTrue(os.path.isfile("./tests/output.vcf"))
-#        # Clean up
-#        print "Cleaning files"
-       # os.system("rm -rf tests/output.vcf testJobTree")
+    def testMarginAlignReadModel(self):
+        # Check if input.hmm exists
+        self.assertTrue(os.path.isfile("./tests/input.hmm"))
+        print "Running marginAlign and reading model from input.hmm"
+        os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --inputModel tests/input.hmm --jobTree testJobTree")
+       # Validate samfile
+        self.validateSam("./tests/test.sam")
+        # Clean up
+        print "Cleaning files"
+        os.system("rm -rf tests/test.sam testJobTree")
 
-#    def testMarginCallerNoMarginalize(self):
-#        # Validate input.sam
-#        print "Running marginCaller without marginalize option"
-#        self.validateSam("./tests/input.sam")
-#        os.system("./marginCaller tests/input.sam tests/reference.fa tests/output.vcf --noMargin --jobTree testJobTree")
-#        # Clean up
-#        print "Cleaning files"
-#        os.system("rm -rf tests/output.vcf testJobTree")
-#
+    def testMarginCaller(self):
+        # Validate input.sam
+        self.validateSam("./tests/input.sam")
+        print "Running marginCaller"
+        os.system("./marginCaller tests/input.sam tests/reference.fa tests/output.vcf --jobTree testJobTree")
+        self.assertTrue(os.path.isfile("./tests/output.vcf"))
+        # Clean up
+        print "Cleaning files"
+      # os.system("rm -rf tests/output.vcf testJobTree")
+
+    def testMarginCallerNoMarginalize(self):
+        # Validate input.sam
+        print "Running marginCaller without marginalize option"
+        self.validateSam("./tests/input.sam")
+        os.system("./marginCaller tests/input.sam tests/reference.fa tests/output.vcf --noMargin --jobTree testJobTree")
+        # Clean up
+        print "Cleaning files"
+        os.system("rm -rf tests/output.vcf testJobTree")
+
 
 
 
