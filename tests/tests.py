7178cd867c41242d1652a8e5f02b5e5053a00ba4
Miten Jain <miten@soe.ucsc.edu>
2015-02-25 10:42:58 -0800

    tests for 2 references

--- tests/tests.py
+++ tests/tests.py
@@ -36,41 +36,76 @@ class TestCase(unittest.TestCase):
         else:
             print "Samfile validation failed"
 
-    def testMarginAlignNoEm(self):
-        self.setUp()
-        print "\nRunning marginAlign with no EM"
+    def testMarginAlignNoEm1Ref(self):
+        print "\nRunning marginAlign for 1 reference, and a pre-trained model (no EM)"
         os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --jobTree testJobTree")
         # Validate samfile
         self.validateSam("./tests/test.sam")
 
-    def testMarginAlignEm(self):
-        print "\nRunning marginAlign with EM and writing output.hmm"
+    def testMarginAlignEm1Ref(self):
+        print "\nRunning marginAlign for 1 reference, with EM and writing output.hmm"
         os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --em --outputModel tests/output.hmm --jobTree testJobTree")
         # Validate samfile
         self.validateSam("./tests/test.sam")
         # Check if output.hmm files were written
         self.assertTrue(os.path.isfile("./tests/output.hmm"))
 
-    def testMarginAlignReadModel(self):
+    def testMarginAlignReadModel1Ref(self):
         # Check if input.hmm exists
         self.assertTrue(os.path.isfile("./tests/input.hmm"))
-        print "\nRunning marginAlign and reading model from input.hmm"
+        print "\nRunning marginAlign using for 1 reference, and reading model from user-provided model input.hmm"
         os.system("./marginAlign tests/reads.fq tests/reference.fa tests/test.sam --inputModel tests/input.hmm --jobTree testJobTree")
         # Validate samfile
         self.validateSam("./tests/test.sam")
 
-    def testMarginCaller(self):
-        print "\nRunning marginCaller"
+    def testMarginCaller1Ref(self):
+        print "\nRunning marginCaller using for 1 reference, and marginalization"
         # Validate input.sam
-        self.validateSam("./tests/input.sam")
-        os.system("./marginCaller tests/input.sam tests/reference.fa tests/output.vcf --jobTree testJobTree")
+        self.validateSam("./tests/input1Ref.sam")
+        os.system("./marginCaller tests/input1Ref.sam tests/reference.fa tests/output.vcf --jobTree testJobTree")
         self.assertTrue(os.path.isfile("./tests/output.vcf"))
 
-    def testMarginCallerNoMarginalize(self):
-        print "\nRunning marginCaller without marginalize option"
+    def testMarginCallerNoMarginalize1Ref(self):
+        print "\nRunning marginCaller for 1 reference, with no marginalize option"
         # Validate input.sam
-        self.validateSam("./tests/input.sam")
-        os.system("./marginCaller tests/input.sam tests/reference.fa tests/output.vcf --noMargin --jobTree testJobTree")
+        self.validateSam("./tests/input1Ref.sam")
+        os.system("./marginCaller tests/input1Ref.sam tests/reference.fa tests/output.vcf --noMargin --jobTree testJobTree")
+
+    def testMarginAlignNoEm2Refs(self):
+        print "\nRunning marginAlign for 2 references, and a pre-trained model (no EM)"
+        os.system("./marginAlign tests/reads.fq tests/references.fa tests/test.sam --jobTree testJobTree")
+        # Validate samfile
+        self.validateSam("./tests/test.sam")
+
+    def testMarginAlignEm2Refs(self):
+        print "\nRunning marginAlign for 2 references, with EM and writing output.hmm"
+        os.system("./marginAlign tests/reads.fq tests/references.fa tests/test.sam --em --outputModel tests/output.hmm --jobTree testJobTree")
+        # Validate samfile
+        self.validateSam("./tests/test.sam")
+        # Check if output.hmm files were written
+        self.assertTrue(os.path.isfile("./tests/output.hmm"))
+
+    def testMarginAlignReadModel2Refs(self):
+        # Check if input.hmm exists
+        self.assertTrue(os.path.isfile("./tests/input.hmm"))
+        print "\nRunning marginAlign using for 2 references, and reading model from user-provided model input.hmm"
+        os.system("./marginAlign tests/reads.fq tests/references.fa tests/test.sam --inputModel tests/input.hmm --jobTree testJobTree")
+        # Validate samfile
+        self.validateSam("./tests/test.sam")
+
+    def testMarginCaller2Refs(self):
+        print "\nRunning marginCaller using for 2 references, and marginalization"
+        # Validate input.sam
+        self.validateSam("./tests/input2Refs.sam")
+        os.system("./marginCaller tests/input2Refs.sam tests/references.fa tests/output.vcf --jobTree testJobTree")
+        self.assertTrue(os.path.isfile("./tests/output.vcf"))
+
+    def testMarginCallerNoMarginalize2Refs(self):
+        print "\nRunning marginCaller for 2 references, with no marginalize option"
+        # Validate input.sam
+        self.validateSam("./tests/input2Refs.sam")
+        os.system("./marginCaller tests/input2Refs.sam tests/references.fa tests/output.vcf --noMargin --jobTree testJobTree")
+
 
 
 if __name__ == '__main__':
